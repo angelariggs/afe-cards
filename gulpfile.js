2f00fd541a850b23bb1b1d15f7764fd3b6383605
[angela riggs] <riggs.ang@gmail.com>
2015-09-16 19:53:59 -0700

    added gulpfile and node dependencies

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var minifyCSS = require('gulp-minify-css');
+var sass = require('gulp-sass');
+var rename = require('gulp-rename');
+var notify = require('gulp-notify');
+
+gulp.task('compileSass', function() {
+	gulp.src('./scss/*.scss')
+	.pipe(sass()
+	.on('error', function(err) {
+		notify().write(err);
+		this.emit('end');
+	}))
+	.pipe(gulp.dest('./css'))
+	.pipe(notify('It worked!'));
+});
+
+gulp.task('serve', function() {
+	gulp.watch('./scss/**/*.scss', ['compileSass']);
+});
+
+gulp.task('build', ['compileSass']);
